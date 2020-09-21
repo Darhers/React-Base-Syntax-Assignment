31147ca42dc1d81a51e77f0771430efa2386153b
Dar Hershkovits <darhers89@gmail.com>
2020-09-20 23:33:31 -0400

    Add two components (UserInput & UserOutput). Import components to app.js and render instances of each component

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const UserOutput = () => {
+    return (
+        <div>
+            <p>
+                Hello!
+            </p>
+            <p>
+                Hello as well!
+            </p>
+        </div>
+    );
+}
+
+export default UserOutput;
