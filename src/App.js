06da248fb40900aa49cb6c55dff5079b2d5c915d
Dar Hershkovits <darhers89@gmail.com>
2020-09-20 23:39:05 -0400

    add state to app.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import UserOutput from './UserOutput/UserOutput'
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [
+      {name: "Dar Hershkovits"},
+      {name: "Hila Hershkovits"},
+      {name: "Alon Hershkovits"}
+    ]
+  };
+
   render() {
     return(
       <div className="App">
         <UserInput />
-        <UserOutput name="Dar" />
-        <UserOutput name="Hila" />
-        <UserOutput name="Alon" />
+        <UserOutput name={this.state.users[0].name} />
+        <UserOutput name={this.state.users[1].name} />
+        <UserOutput name={this.state.users[2].name} />
       </div>
     )
   }
